aa7b38d8731cced90f1eac6d73911c6b1d3d9deb
Sibusile Makhathini <1113969@students.wits.ac.za>
2021-01-04 16:18:30 +0200

    Exercise 3.3

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -5,21 +5,46 @@
 #include <complex>	// contains the complex class definition
 
 using namespace std; // cout, endl, complex are all part of this namespace
+using complex_float = complex<float>;
+
+float delta(float& a, float& b, float& c)
+{
+    return b*b - 4*a*c;
+}
+
+void print_ans(complex_float x1, complex_float x2)
+{
+
+ 	cout << "The answer is: " << x1 << "or " << x2<< endl;
+	cout << "Or in a more familiar form: " << x1.real()
+			<< " + " << x1.imag() << "j" << " or " << x2.real()
+             << x2.imag() << "j"
+			<< endl	<< endl;
+}
 
 int main()
 {
-	auto num1 = complex<float>{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
+    float a, b, c;
+    char char_;
+    complex_float x1, x2;
 
-	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
+    while(char_ != 'q')
+    {
+    cout << "Want to calculate Yes/Quit (y/q)?" << endl;
+    cin >> char_;
+    if(char_ == 'q')
+        return 0;
+    cout << "Please in put value a, b, c in this order:" << endl;
+    cin >> a >> b >> c;
 
-	cout << "The answer is: " << answer << endl;
-	cout << "Or in a more familiar form: " << answer.real() 
-			<< " + " << answer.imag() << "j" 
-			<< endl	<< endl;
+    auto delta_ = delta(a, b, c);
 
-	// answer++;
+    x1 = (-b + sqrt((complex_float)delta_))/(2*a);
+    x2 = (-b - sqrt((complex_float)delta_))/(2*a);
 
+    print_ans(x1, x2);
+    cout << endl;
+    }
 	return 0;
 }
 
