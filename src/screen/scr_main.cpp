794d195317ec65585d5dc7be9b38379034f8b604
Sibusile Makhathini <1113969@students.wits.ac.za>
2021-01-05 09:57:28 +0200

    Exercise 4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -7,24 +7,13 @@ using namespace std;
 int main()
 {
 	auto myScreen = Screen{6,6};
-	myScreen.forward();
-	myScreen.set('*');
-	myScreen.down();
-	myScreen.set('*');
-	myScreen.move(3,3);
-	myScreen.set("---");
-
-	myScreen.display();
-	cout << endl;
-
-	myScreen.reSize(16,16);
-	myScreen.display();
-	myScreen.clear(' ');
-
-	myScreen.move(7,7);
-	myScreen.set("BIG");
-	myScreen.move(8,5);
-	myScreen.set("SCREEN");
+	myScreen.clear('*');
+	myScreen.move(2,2);
+	myScreen.set("     ");
+	myScreen.move(3,2);
+	myScreen.set("     ");
+    myScreen.move(5,1);
+	myScreen.set("     ");
 	myScreen.display();
 
 	return 0;
